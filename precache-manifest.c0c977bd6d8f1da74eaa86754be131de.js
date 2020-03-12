fae5e162c142268411db04390490d9258506b3e4
name from github <zlatko.gornjevski@gmail.com>
2020-03-12 15:53:04 +0100

    Updates

---
+++ precache-manifest.c0c977bd6d8f1da74eaa86754be131de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790daf6920cffc45ef990bfccf74c180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "11a985b0608d976648d7",
+    "url": "/static/css/main.43018e1e.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a985b0608d976648d7",
+    "url": "/static/js/main.e68c3d6e.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);

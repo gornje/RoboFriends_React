9118a5e0527ae0c7f8cf4dc1be3422bec3d1d032
name from github <zlatko.gornjevski@gmail.com>
2020-03-12 17:09:35 +0100

    removed error catching

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,6 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
     constructor() {
@@ -40,16 +39,12 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <ErrorBoundry>
                             <CardList robots={filteredRobots} />
-                        </ErrorBoundry>
                     </Scroll>
                 </div>
             )
     }
 
 }
-
-
 
 export default App;
